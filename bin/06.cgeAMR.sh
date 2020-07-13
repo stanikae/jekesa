880a46a9411ac2ca42c07c76beea0b8b21f9a75b
Stanford Kwenda <stankwenda@gmail.com>
2020-07-13 15:02:09 +0200

    Combine pointfinder and resfinder sample results

--- bin/06.cgeAMR.sh
+++ bin/06.cgeAMR.sh
@@ -27,17 +27,48 @@ for contigs in $(find $spadesDir -name "*_assembly.fasta")
   -s salmonella -m blastn -m_p ${CONDA_BASE}/envs/resfinder/bin/blastn \
   -t 0.90 -l 0.60 >> $project/tmp/06.pointfinder.log 2>&1
   # prepare output file for each sample
+  #sed -i -n '/Known Mutations/,$p' $pointfinder/$name/${name}_blastn_HTMLtable.txt
+  # echo $name | cat - $pointfinder/$name/${name}_blastn_HTMLtable.txt | \
+  # sed '/^[[:space:]]*$/d' | tr '\t' ';' > $pointfinder/${name}_pointfinder.txt
+   # add line with gene IDs
   sed -i -n '/Known Mutations/,$p' $pointfinder/$name/${name}_blastn_HTMLtable.txt
-  echo $name | cat - $pointfinder/$name/${name}_blastn_HTMLtable.txt | \
-  sed '/^[[:space:]]*$/d' | tr '\t' ';' > $pointfinder/${name}_pointfinder.txt
+  sed -i '1d' $pointfinder/$name/${name}_blastn_HTMLtable.txt
+  header=$(cat $pointfinder/$name/${name}_blastn_HTMLtable.txt | \
+  sed 's/^[[:space:]]*$/:/' | sed '/^Mutation/d' | sed 's/\t/;/g' | \
+  awk 'BEGIN{FS="\n";ORS="|"} {print $1}' | awk 'BEGIN{FS="|";RS=":"} {print $2}' | \
+  sed '/^$/d' | awk -v RS="\n" -v ORS="|" '1' | sed 's/|$/\n/g')
+  # 
+  printf "sampleID|$header\n" > $pointfinder/${name}_pointfinder.txt
+  # add mutations line
+  info=$(cat $pointfinder/$name/${name}_blastn_HTMLtable.txt | \
+  sed 's/^[[:space:]]*$/:/' | sed '/^Mutation/d' | sed 's/\t/;/g' | \
+  awk 'BEGIN{FS="\n";ORS="|"} {print $1}' | awk 'BEGIN{FS="|";RS=":"} {print $3}' | \
+  sed '/^$/d' | awk -v RS="\n" -v ORS="|" '1' | sed 's/|$/\n/g')
+  #
+  printf "$name|$info\n" >> $pointfinder/${name}_pointfinder.txt
 done
 # deactivate resfinder environment
 conda deactivate
 
 # merge resfinder output from multiple samples
-for file in $(find ~/Test-pipeline-02/resfinder -name "*.resfindr.tsv"); do 
+for file in $(find $resfinder -name "*.resfindr.tsv"); do 
   cat $file >> $resfinder/06.resfinder.tsv
 done
 # remove multiple headers
 sed -i '1!{/^sampleID/d;}' $resfinder/06.resfinder.tsv
 
+# combine pointfinder results
+for file in $(find $pointfinder -name "*_pointfinder.txt"); do
+  cat $file >> $pointfinder/06.pointfinder.tsv
+done
+# remove multiple headers
+sed -i '1!{/^sampleID/d;}' $pointfinder/06.pointfinder.tsv 
+sed -i 's/|/\t/g' $pointfinder/06.pointfinder.tsv
+
+# convert resfinder .tsv to .xlsx
+Rscript $SCRIPTS_DIR/tsv2xlsx.R $resfinder/06.resfinder.tsv \
+$reportsDir/06.resfinder.xlsx >> $project/tmp/06.resfinder.tsv2xlsx.log 2>&1
+
+# convert pointfinder .tsv to .xlsx
+Rscript $SCRIPTS_DIR/tsv2xlsx.R $pointfinder/06.pointfinder.tsv \
+$reportsDir/06.pointfinder.xlsx >> $project/tmp/06.pointfinder.tsv2xlsx.log 2>&1
