305aa32923f43a300bf8b40ea0dbc4bd8465b1bd
stanikae <stankwenda@gmail.com>
2020-07-24 08:53:42 +0200

    Add contigs input option

--- bin/06.resfinder2tsv.R
+++ bin/06.resfinder2tsv.R
@@ -2,7 +2,7 @@
 #args = commandArgs(trailingOnly=TRUE)
 
 
-library(tidyverse)
+library("tidyverse")
 library("openxlsx")
 
 args <- commandArgs(TRUE)
@@ -10,8 +10,12 @@ args <- commandArgs(TRUE)
 # define path to tsv files
 indir = file.path(args[1])
 # combine tsvs in one data frame
-tbl <- list.files(path = indir,pattern = "*.tsv", full.names = T) %>% 
-       map_df(~read_delim(., delim = "\t"))
+#tbl <- list.files(path = indir,pattern = "*.tsv", full.names = T) %>% 
+#       map_df(~read_delim(., delim = "\t"))
+
+all_tsv <- lapply(list.files(path = indir,pattern = "*.tsv", full.names = T), 
+             read_delim, delim="\t")
+tbl <- do.call("rbind", all_tsv)
 # write tsvs to one tsv file
 #write_delim(tbl,args[2],delim = "\t")
 # write tsv to .xlsx
