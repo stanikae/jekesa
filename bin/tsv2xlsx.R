b4a4482fa710886ce65efb360ab53a338be8a96f
stanikae <stankwenda@gmail.com>
2020-10-06 15:35:55 +0200

    Set asTable parameter to TRUE

--- bin/tsv2xlsx.R
+++ bin/tsv2xlsx.R
@@ -1,18 +1,5 @@
 #!/usr/bin/env Rscript
 #args = commandArgs(trailingOnly=TRUE)
-
-
-#library(plyr)
-#library(tidyverse)
-#library(plyr)
-#library(tidyr)
-#library(dplyr)
-#library(purrr)
-#library(rJava)
-#library(XLConnectJars)
-#library(XLConnect)
-#library(stringr)
-
 #.libPaths("~/repos/jekesa/lib/Rlib")
 
 library("openxlsx")
@@ -21,12 +8,7 @@ args <- commandArgs(TRUE)
 print(args[1])
 print(args[2])
 
-#setwd(args)
-#getwd()
 path = file.path(args[1])
 cat("Path is: ", path,"\n")
-#print(path)
-#library("openxlsx")
-#write.xlsx(read.delim(args[2]), paste(path, "mlst_typing_results.xlsx", sep = "/"))
-write.xlsx(read.delim(args[1], header = TRUE), args[2])
+write.xlsx(read.delim(args[1], header = TRUE), args[2], asTable = T, overwrite=T)
 
