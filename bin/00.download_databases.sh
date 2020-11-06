0e1436ae1413d6aa3ca767d59786b44393a8eaf8
stanikae <stankwenda@gmail.com>
2020-11-07 01:13:43 +0200

    Add serotypefinder and virulencefinder dbs

--- bin/00.download_databases.sh
+++ bin/00.download_databases.sh
@@ -25,7 +25,8 @@ poppunk_gas_db_url="https://s3-eu-west-1.amazonaws.com/pfigshare-u-files/1742414
 resfinder_db_url="https://git@bitbucket.org/genomicepidemiology/resfinder_db.git"
 pointfinder_db_url="https://git@bitbucket.org/genomicepidemiology/pointfinder_db.git"
 disinfinder_db_url="https://git@bitbucket.org/genomicepidemiology/disinfinder_db.git"
-
+serotypefinder_db_url="https://bitbucket.org/genomicepidemiology/serotypefinder_db.git"
+virulencefinder_db_url="https://bitbucket.org/genomicepidemiology/virulencefinder_db.git"
 # function modified from ariba download function 
 db_download () {
   url=$1
@@ -48,31 +49,36 @@ cge_git () {
   if ! [ -d $db_name ]; then
     git clone $url $db_name
     cd $db_name
-    python3 INSTALL.py non_interactive
+    python3 INSTALL.py kma_index non_interactive
   fi  
 }
 
 # ------------------------ MiniKraken ----------------------------
-if ! [ -d $installDir/kraken2_db ]; then
-   mkdir -p $installDir/kraken2_db
+if ! [ -d $installDir/kraken8gb_standard ]; then
+   mkdir -p $installDir/kraken8gb_standard
+   cd $installDir/kraken8gb_standard
+   db_download "${kraken_download_url}" "kraken8gb_standard.tar.gz"
+   tar -zxf kraken8gb_standard.tar.gz
 fi
-cd $installDir/kraken2_db
-db_download "${kraken_download_url}" "kraken8gb_standard.tar.gz"
-tar -zxf kraken8gb_standard.tar.gz
-ln -nsf $installDir/kraken2_db $DBPATH/kraken_db
+#
+if ! [ -h $DBPATH/kraken_db ]; then
+ ln -nsf $installDir/kraken8gb_standard $DBPATH/kraken_db
+fi
 # ----- PopPunk S. pneumoniae and S. pyogenes databases ----------
 if ! [ -d $installDir/poppunk_db ]; then
    mkdir -p $installDir/poppunk_db
+   cd $installDir/poppunk_db
+   db_download "${poppunk_gpsc_db_url}" "GPS_query.tar.bz2"
+   db_download "${poppunk_gpsc_url}" "gpsc_definitive.csv"
+   db_download "${poppunk_gas_db_url}" "GAS_query_v2.tar.bz2"
+
+   tar -jxf GAS_query_v2.tar.bz2
+   tar -jxf GPS_query.tar.bz2
 fi
-cd $installDir/poppunk_db
-db_download "${poppunk_gpsc_db_url}" "GPS_query.tar.bz2"
-db_download "${poppunk_gpsc_url}" "gpsc_definitive.csv"
-db_download "${poppunk_gas_db_url}" "GAS_query_v2.tar.bz2"
-
-tar -jxf GAS_query_v2.tar.bz2
-tar -jxf GPS_query.tar.bz2
-
-ln -nsf $installDir/poppunk_db $DBPATH/poppunk_db
+#
+if ! [ -h $DBPATH/poppunk_db ]; then
+   ln -nsf $installDir/poppunk_db $DBPATH/poppunk_db
+fi
 # -------------------------- ConFindr ----------------------------
 echo "To download ConFindr databases, kindly follow the instructions"
 echo "at https://olc-bioinformatics.github.io/ConFindr/install/#downloading-confindr-databases"
@@ -87,14 +93,39 @@ export PATH="$CONDA_BASE/envs/cge/bin:$PATH"
 
 cd $installDir
 cge_git "${resfinder_db_url}" db_resfinder
-ln -nsf $installDir/db_resfinder $DBPATH/resfinder_db
+#
+if ! [ -h $DBPATH/resfinder_db ]; then
+ ln -nsf $installDir/db_resfinder $DBPATH/resfinder_db
+fi
 # --------------------------- PointFinder ------------------------
 cd $installDir
 cge_git "${pointfinder_db_url}" db_pointfinder
-ln -nsf $installDir/db_pointfinder $DBPATH/pointfinder_db
+#
+if ! [ -h $DBPATH/pointfinder_db ]; then
+ ln -nsf $installDir/db_pointfinder $DBPATH/pointfinder_db
+fi
 # --------------------------- DisinFinder -------------------------
 cd $installDir
 cge_git "${disinfinder_db_url}" db_disinfinder
-ln -nsf $installDir/db_disinfinder $DBPATH/disinfinder_db
+#
+if ! [ -h $DBPATH/disinfinder_db ]; then
+  ln -nsf $installDir/db_disinfinder $DBPATH/disinfinder_db
+fi
 # --------------------------- ResFinder4 -------------------------
 
+# --------------------------- SeroTyperFinder --------------------
+cd $installDir
+cge_git "${serotypefinder_db_url}" db_stfinder
+#
+if ! [ -h $DBPATH/db_stfinder ]; then 
+ ln -nsf $installDir/db_stfinder $DBPATH/db_stfinder
+fi
+# --------------------------- VirulenceFinder --------------------
+cd $installDir
+cge_git "${virulencefinder_db_url}" db_virulencefinder
+#
+if ! [ -h $DBPATH/db_virulencefinder ]; then
+ ln -nsf $installDir/db_virulencefinder $DBPATH/db_virulencefinder
+fi
+# ----------------------------------------------------------------
+conda deactivate
